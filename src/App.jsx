7ab9dd7af73a7b4292ba28c485cd6a7c55f97cac
farrel irawan <irawanssfarrel@gmail.com>
2025-03-28 01:06:33 +0700

    install typed.js dan membuat hero section menjadi reusable component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Section from "./components/Section"
+import Hero from "./components/Hero"
 
 function App() {
 
@@ -6,27 +7,10 @@ function App() {
     <>
       <main>
         <Section sectionId='home'>
-          <div className="flex items-center justify-center md:flex-wrap md:gap-7 lg:flex-nowrap">
-              <div className="w-1/2 flex flex-col gap-3 md:w-full md:items-center lg:items-start lg:w-1/2">
-                <p className="text-sm font-main font-semibold text-secondary capitalize">hitung bunga jadi lebih cepat pake bunga gampang!</p>
-                <h2 className="font-mainBold text-3xl font-bold capitalize leading-tight max-w-xl md:text-center lg:text-left lg:text-5xl lg:max-w-2xl lg:leading-tight">
-                  mulai prediksi uang dengan bunga anda untuk <span className="text-primary">masa mendatang.</span>
-                </h2>
-                <h3 className="text-lg font-main font-semibold text-secondary max-w-lg leading-tight md:text-center lg:text-left">kami, bunga gampang siap membantu anda dengan perhitungan yang cepat dan akurat.</h3>
-                <div className="flex items-center gap-3 mt-5">
-                  <a href="#">
-                    <button className="px-3 py-2 bg-primary rounded-lg text-text capitalize font-medium text-sm leading-relaxed transition-colors duration-300 ease-out hover:bg-primary/90">mulai hitung</button>
-                  </a>
-                  <hr className="border border-black h-5"/>
-                  <a href="#">
-                    <button className="px-3 py-2 bg-secondary rounded-lg text-text capitalize font-medium text-sm leading-relaxed transition-colors duration-300 ease-out hover:bg-secondary/90">pilih metode perhitungan</button>
-                  </a>
-                </div>
-              </div>
-              <div className="w-1/2 bg-green-600 flex justify-end md:w-full md:items-center lg:items-start lg:w-1/2">
-                <img src="https://placehold.co/500x500" alt="" className="bg-red-500"/>
-              </div>
-            </div>
+          <Hero />
+        </Section>
+        <Section sectionId='about'>
+          
         </Section>
       </main>
     </>
