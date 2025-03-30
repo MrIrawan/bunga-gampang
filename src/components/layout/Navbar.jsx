a7b9a25ccfb34c49bbbd0a626fd7569859b124bf
farrel irawan <irawanssfarrel@gmail.com>
2025-03-30 08:53:44 +0700

    membuat komponen layout navbar

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,37 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faBars } from "@fortawesome/free-solid-svg-icons";
+
+export default function Navbar() {
+    return(
+        <header className="w-full shadow-xl">
+            <nav className="w-4/5 mx-auto flex items-center justify-between">
+                <div className="p-5 w-1/2 flex items-center justify-start gap-10">
+                    <div className="w-fit">
+                        <h2 className="text-xl font-mainBold text-primary leading-tight">Bunga <span className="block">Gampang.</span></h2>
+                    </div>
+                    <div className="w-fit">
+                        <ul className="flex items-center gap-5">
+                            <li className="font-main text-base leading-relaxed transition-all duration-300 ease-in-out">
+                                <a href="#">Beranda</a>
+                            </li>
+                            <li className="font-main text-base leading-relaxed transition-all duration-300 ease-in-out">
+                                <a href="#">Tentang</a>
+                            </li>
+                            <li className="font-main text-base leading-relaxed transition-all duration-300 ease-in-out">
+                                <a href="#">Mulai Hitung</a>
+                            </li>
+                            <li className="font-main text-base leading-relaxed transition-all duration-300 ease-in-out">
+                                <a href="#">Blog</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="p-5 w-1/2 flex items-center justify-end">
+                    <div className="w-fit cursor-pointer">
+                        <FontAwesomeIcon icon={faUser} className="text-2xl"/>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
