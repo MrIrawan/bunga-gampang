8bc1a9b1bab4750d27f08000e5087ebe336a7746
farrel irawan <irawanssfarrel@gmail.com>
2025-04-03 03:14:23 +0700

    modifikasi footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedinIn, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
     return(
@@ -7,35 +8,82 @@ export default function Footer() {
             <div className="container w-full mx-auto">
                 <div className="w-full flex">
                     <article className="p-5 w-full">
-                        <h2 className="text-3xl font-mainBold text-primary leading-tight">Bunga <span className="block">Gampang.</span></h2>
+                        <h2 className="text-3xl font-mainBold text-primary leading-tight w-fit mx-auto">Bunga <span className="block">Gampang.</span></h2>
                     </article>
                     <article className="p-5 w-full">
-                        <h3 className="text-xl font-mainBold text-primary leading-relaxed">Linked</h3>
-                        <div className="w-full">
-                            <ul className="flex gap-10 items-center">
-                                <div className="flex flex-col gap-2 my-3">
-                                    <li className="text-black font-main text-base leading-relaxed">
-                                        <a href="#">Beranda</a>
-                                    </li>
-                                    <li className="text-black font-main text-base leading-relaxed">
-                                        <a href="#">Tentang</a>
-                                    </li>
-                                </div>
-                                <div className="flex flex-col gap-2 my-3">
-                                    <li className="text-black font-main text-base leading-relaxed">
-                                        <a href="#">Mulai Hitung</a>
-                                    </li>
-                                    <li className="text-black font-main text-base leading-relaxed">
-                                        <a href="#">Blog</a>
-                                    </li>
-                                </div>
-                            </ul>
+                        <div className="w-fit mx-auto">
+                            <h3 className="text-xl font-mainBold text-primary leading-relaxed">Linked</h3>
+                            <div className="w-full mt-3">
+                                <ul className="flex gap-10 items-center">
+                                    <div className="flex flex-col gap-2">
+                                        <li className="text-black font-main text-base leading-relaxed">
+                                            <a href="#">Beranda</a>
+                                        </li>
+                                        <li className="text-black font-main text-base leading-relaxed">
+                                            <a href="#">Tentang</a>
+                                        </li>
+                                    </div>
+                                    <div className="flex flex-col gap-2">
+                                        <li className="text-black font-main text-base leading-relaxed">
+                                            <a href="#">Mulai Hitung</a>
+                                        </li>
+                                        <li className="text-black font-main text-base leading-relaxed">
+                                            <a href="#">Blog</a>
+                                        </li>
+                                    </div>
+                                </ul>
+                            </div>
                         </div>
                     </article>
                     <article className="p-5 w-full">
-                        <h3 className="text-xl font-mainBold text-primary leading-relaxed">Connect</h3>
-                        
+                        <div className="w-fit mx-auto">
+                            <h3 className="text-xl font-mainBold text-primary leading-relaxed">Connect</h3>
+                            <div className="w-full mt-3">
+                                <ul className="flex flex-col gap-4">
+                                    <li>
+                                        <div className="flex items-center gap-3">
+                                            <FontAwesomeIcon icon={faInstagram} className="text-xl"/>
+                                            <div className="flex flex-col gap-1">
+                                                <h4 className="font-mainBold text-sm leading-relaxed">@canttguardrell</h4>
+                                                <p className="font-main text-xs">Connect with instagram</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex items-center gap-3">
+                                            <FontAwesomeIcon icon={faGithub} className="text-xl"/>
+                                            <div className="flex flex-col gap-1">
+                                                <h4 className="font-mainBold text-sm leading-relaxed">MrIrawan</h4>
+                                                <p className="font-main text-xs">Connect with github</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex items-center gap-3">
+                                            <FontAwesomeIcon icon={faLinkedinIn} className="text-xl"/>
+                                            <div className="flex flex-col gap-1">
+                                                <h4 className="font-mainBold text-sm leading-relaxed">Farrel Irawan</h4>
+                                                <p className="font-main text-xs">Connect with linkedin</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div className="flex items-center gap-3">
+                                            <FontAwesomeIcon icon={faEnvelope} className="text-xl"/>
+                                            <div className="flex flex-col gap-1">
+                                                <h4 className="font-mainBold text-sm leading-relaxed">irawanssfarrel@gmail.com</h4>
+                                                <p className="font-main text-xs">Get in touch</p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
                     </article>
+                </div>
+                <div className="border-t py-5 flex items-center justify-between mt-6">
+                    <p className="font-mainBold text-sm">&copy; 2025 Bunga Gampang. All rights reserved.</p>
+                    <p className="font-main text-sm">Built with Farrel Irawan, with passion and precision.</p>
                 </div>
             </div>
         </footer>
