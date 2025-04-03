7689497b1e0c33d214f4bcbb2a9fe3e5180071da
farrel irawan <irawanssfarrel@gmail.com>
2025-04-04 02:32:56 +0700

    modifikasi pada sidebar

--- src/pages/Count.jsx
+++ src/pages/Count.jsx
@@ -14,11 +14,11 @@ export default function Count() {
     return(
         <>
             <aside 
-                className={`w-[30%] ${isClicked ? 'translate-x-0' : '-translate-x-52'} h-[900px] shadow-md border transition-transform duration-300 ease-out lg:w-[20%]`}
+                className={`w-[400px] ${isClicked ? 'translate-x-0' : '-translate-x-72'} h-[900px] shadow-md border transition-transform duration-300 ease-out lg:w-[20%]`}
                 onClick={() => clickHandlerToAside()}
             >
                 {isClicked === true && 
-                    <div className={`w-full flex items-center justify-between px-3 flex-wrap gap-4 mt-4`}>
+                    <div className={`w-full flex items-center justify-between px-10 flex-wrap gap-4 my-4`}>
                         <h3 className="text-2xl font-mainBold text-primary leading-relaxed">BG.</h3>
                         <FontAwesomeIcon icon={faChevronRight} className="w-5 h-5 rotate-180"/>
                         <hr className="w-full border border-secondary/30"/>
@@ -27,12 +27,30 @@ export default function Count() {
                 <div className={`w-full py-2 flex ${isClicked ? 'flex-col items-center' : 'justify-end'}`}>
                     {isClicked === true && 
                         <>
-                        
+                            <div className="w-full px-10 flex flex-col gap-6">
+                                <h2 className="text-xl font-mainBold text-primary leading-relaxed capitalize">kategori perhitungan</h2>
+                                <div className="w-full">
+                                    <ul className="flex flex-col gap-3 border-l border-l-secondary/30 pl-5">
+                                        <li className="text-lg font-main leading-relaxed hover-list cursor-pointer">
+                                            contoh bunga
+                                        </li>
+                                        <li className="text-lg font-main leading-relaxed hover-list cursor-pointer">
+                                            contoh bunga
+                                        </li>
+                                        <li className="text-lg font-main leading-relaxed hover-list cursor-pointer">
+                                            contoh bunga
+                                        </li>
+                                        <li className="text-lg font-main leading-relaxed hover-list cursor-pointer">
+                                            contoh bunga
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </>
                     }
                     {isClicked === false && 
                         <>
-                            <div className="w-fit mr-3 pt-4 flex flex-col gap-3 items-center">
+                            <div className="w-fit mr-8 pt-4 flex flex-col gap-3 items-center lg:mr-6">
                                 <h3 className="text-2xl font-mainBold text-primary">BG.</h3>
                                 <hr className="border border-secondary/30 w-full"/>
                                 <FontAwesomeIcon icon={faChevronRight} className="w-5 h-5"/>
