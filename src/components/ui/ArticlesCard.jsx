760d3a50b173d0fa428926b44a365d100ce798c6
farrel irawan <irawanssfarrel@gmail.com>
2025-04-15 17:16:04 +0700

    membuat komponen articles card untuk menampilkan data artikel

---
+++ src/components/ui/ArticlesCard.jsx
@@ -0,0 +1,30 @@
+export default function ArticlesCard({ variant = 'default' }) {
+    return (
+        <>
+            <div className="shadow-lg border w-full h-[400px] flex lg:w-3/4">
+                <article className="w-3/5 h-[400px] overflow-hidden">
+                    <img src="/images/saham.jpg" alt="" className="w-full h-full object-cover"/>
+                </article>
+                <article className="w-full py-3 px-6">
+                    <div className="w-fit flex gap-3 items-center mb-4 mt-3">
+                        <div className="flex items-center gap-2">
+                            <div className="w-8 h-8 bg-red-100 rounded-full overflow-hidden">
+                                <img src="https://placehold.co/600x400" alt="" className="w-full h-full object-cover"/>
+                            </div>
+                            <p className="text-sm font-main font-semibold text-secondary">Posted By Admin</p>
+                        </div>
+                        <span className="w-2 h-2 bg-primary rounded-full"></span>
+                        <p className="text-sm font-main text-secondary font-semibold">On 13 April 2025</p>
+                    </div>
+                    <div className="w-full flex flex-col mb-4">
+                        <h2 className="text-2xl font-mainBold mb-4 leading-tight">Bagaimana Cara Memulai saham untuk pemula?</h2>
+                        <p className="text-sm font-main text-secondary font-semibold lg:max-w-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam labore velit nobis! Fugiat cum eius, doloribus mollitia obcaecati at reprehenderit.</p>
+                    </div>
+                    <div className="w-full lg:max-w-md">
+                        
+                    </div>
+                </article>
+            </div>
+        </>
+    );
+}
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,5 +1,6 @@
 import { categories } from '../../public/categories.js'
 import CategoriesBar from '../components/ui/CategoriesBar.jsx'
+import ArticlesCard from '../components/ui/ArticlesCard.jsx'
 
 export default function Blog() {
     return (
@@ -7,6 +8,14 @@ export default function Blog() {
             <div className="container w-full mx-auto">
                 <div className='w-full flex flex-col gap-6'>
                     <CategoriesBar data={categories} />
+                </div>
+                <div className="w-full py-1">
+                    <div id='latest' className='w-full p-3'>
+                        <h2 className='text-2xl mb-3 font-mainBold leading-relaxed capitalize'>latest post</h2>
+                        <div className='w-full flex flex-wrap'>
+                            <ArticlesCard />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
