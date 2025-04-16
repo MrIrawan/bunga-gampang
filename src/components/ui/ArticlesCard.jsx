f63304e02274410a9e9a9b0650a2d8935b9dd887
farrel irawan <irawanssfarrel@gmail.com>
2025-04-16 17:00:58 +0700

    menyelesaikan article card

--- src/components/ui/ArticlesCard.jsx
+++ src/components/ui/ArticlesCard.jsx
@@ -1,11 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+
 export default function ArticlesCard({ variant = 'default' }) {
     return (
         <>
-            <div className="shadow-lg border w-full h-[400px] flex lg:w-3/4">
-                <article className="w-3/5 h-[400px] overflow-hidden">
+            <div className="shadow-lg border w-full h-[400px] flex lg:w-full">
+                {/* article image */}
+                <article className="w-3/5 h-full overflow-hidden lg:w-4/5">
                     <img src="/images/saham.jpg" alt="" className="w-full h-full object-cover"/>
                 </article>
-                <article className="w-full py-3 px-6">
+                {/* article content */}
+                <article className="w-full py-3 px-6 flex flex-col">
+                    {/* author and date */}
                     <div className="w-fit flex gap-3 items-center mb-4 mt-3">
                         <div className="flex items-center gap-2">
                             <div className="w-8 h-8 bg-red-100 rounded-full overflow-hidden">
@@ -16,10 +22,14 @@ export default function ArticlesCard({ variant = 'default' }) {
                         <span className="w-2 h-2 bg-primary rounded-full"></span>
                         <p className="text-sm font-main text-secondary font-semibold">On 13 April 2025</p>
                     </div>
-                    <div className="w-full flex flex-col mb-4">
+
+                    {/* title and description */}
+                    <div className="w-full flex flex-col mb-5">
                         <h2 className="text-2xl font-mainBold mb-4 leading-tight">Bagaimana Cara Memulai saham untuk pemula?</h2>
                         <p className="text-sm font-main text-secondary font-semibold lg:max-w-md">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam labore velit nobis! Fugiat cum eius, doloribus mollitia obcaecati at reprehenderit.</p>
                     </div>
+
+                    {/* for categories tags */}
                     <div className="w-full lg:max-w-md">
                         <span 
                             className={`border border-secondary/60 py-1 px-5 mx-1 my-1 rounded-full text-base font-main cursor-pointer transition-all duration-200 ease-in-out`}
@@ -36,6 +46,14 @@ export default function ArticlesCard({ variant = 'default' }) {
                         >
                             blockchain
                         </span>
+                    </div>
+
+                    {/* read more button (footer) */}
+                    <div className="w-fit mt-auto mb-2">
+                        <a href="#" className="flex items-center gap-2 text-base font-main hover-list">
+                            Read More
+                            <FontAwesomeIcon icon={faArrowRight} />
+                        </a>
                     </div>
                 </article>
             </div>
