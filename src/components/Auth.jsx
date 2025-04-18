6e04d084777cfa848b78cd35afa013d28ead98b2
farrel irawan <irawanssfarrel@gmail.com>
2025-04-19 03:33:47 +0700

    fetch register ke API

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom";
 
@@ -17,7 +18,36 @@ export default function Auth({ variant }) {
 
 function Register() {
 
-    
+    const [ formData, setFormData ] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    const handleSubmitRegister = async (e) => {
+        e.preventDefault();
+        console.log(formData);
+        
+        try {
+            const response = await fetch("http://localhost:8800/api/daftar", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(formData)
+            });
+
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+
+            const responseData = await response.json();
+
+        } catch (error) {
+            console.log(error);
+        }
+
+    }
 
     return (
         <>
@@ -26,18 +56,36 @@ function Register() {
                 <div className="w-full flex flex-col">
                     <h2 className="text-2xl mb-6 font-mainBold text-black leading-tight lg:text-3xl lg:mx-auto">Halo, Selamat datang di <span className="block text-primary">Bunga Gampang.</span></h2>
                     <div className="w-full h-[400px] lg:w-4/5 lg:mx-auto">
-                        <form action="" className="w-full h-full flex flex-col">
+                        <form onSubmit={(e) => handleSubmitRegister(e)} className="w-full h-full flex flex-col">
                             <div className="w-full flex flex-col gap-1 my-3">
                                 <label htmlFor="name" className="font-mainBold text-sm">Nama Lengkap</label>
-                                <input type="text" name="name" id="name" className="border-b py-2 focus:outline-none focus:border-b-primary font-main text-base"/>
+                                <input 
+                                    type="text" 
+                                    name="name" 
+                                    id="name" 
+                                    className="border-b py-2 focus:outline-none focus:border-b-primary font-main text-base"
+                                    onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+                                />
                             </div>
                             <div className="w-full flex flex-col gap-1 my-3">
                                 <label htmlFor="email" className="font-mainBold text-sm">Email Anda</label>
-                                <input type="email" name="email" id="email" className="border-b py-2 focus:outline-none focus:border-b-primary font-main text-base"/>
+                                <input 
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className="border-b py-2 focus:outline-none focus:border-b-primary font-main text-base"
+                                    onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                                />
                             </div>
                             <div className="w-full flex flex-col gap-1 my-3">
                                 <label htmlFor="password" className="font-mainBold text-sm">Password Email</label>
-                                <input type="password" name="password" id="password" className="border-b py-2 focus:outline-none focus:border-b-primary font-main text-base"/>
+                                <input 
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    className="border-b py-2 focus:outline-none focus:border-b-primary font-main text-base"
+                                    onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                                />
                             </div>
                             <div className="w-full mt-auto">
                                 <button type="submit" className="font-main border w-full mb-6 py-2 bg-primary text-text">Daftar Sekarang</button>
