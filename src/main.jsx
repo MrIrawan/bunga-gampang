64ef3fb5cbec2c1a44f02a9f0dc33a3633a0e98b
farrel irawan <irawanssfarrel@gmail.com>
2025-04-24 16:31:23 +0700

    menambahkan context yang sudah di buat pada file main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom'
 import { routers } from '../routers.jsx'
 import { Toaster } from 'sonner'
 import { watchToken } from './services/tokenServices.js'
+import { AuthContext } from './context/AuthContext.jsx'
 import './index.css'
 import Navbar from './components/layout/Navbar.jsx'
 import Footer from './components/layout/Footer.jsx'
@@ -12,13 +13,15 @@ watchToken();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Navbar />
-      <RouterProvider router={routers} />
-      <Toaster toastOptions={{
-        style: {
-          fontFamily: 'main, times new roman',
-        },
-      }}/>
-    <Footer />
+    <AuthContext>
+      <Navbar />
+        <RouterProvider router={routers} />
+        <Toaster toastOptions={{
+          style: {
+            fontFamily: 'main, times new roman',
+          },
+        }}/>
+      <Footer />
+    </AuthContext>
   </StrictMode>,
 )
