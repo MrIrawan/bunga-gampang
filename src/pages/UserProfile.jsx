066c69270dee5945e1af900e98e061fa5b549374
farrel irawan <irawanssfarrel@gmail.com>
2025-04-25 23:02:21 +0700

    memasukan data user menggunakan context

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -4,6 +4,7 @@ import { AuthContext } from "../context/AuthContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faGear, faLock } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
+import Avatar from "../components/ui/Avatar";
 
 export default function UserProfile() {
 
@@ -43,10 +44,48 @@ export default function UserProfile() {
                         </ul>
                     </div>
                 </aside>
-                <div className="container w-full mx-auto h-full py-5">
+                <div className="container w-full mx-auto h-full py-5 px-7">
                     {/* components here */}
+                    { currentIndex === 0 ? <AccountInfo /> 
+                        :
+                    currentIndex === 1 ? <DataAndPrivacy /> 
+                        :
+                    <SettingAccount /> }
                 </div>
             </section>
         </>
+    )
+}
+
+function AccountInfo() {
+
+    const { state } = useContext(AuthContext);
+
+    return (
+        <>
+            <h1 className="text-3xl font-mainBold text-primary mt-3 mb-10">Profil Kamu</h1>
+            <div className="w-full flex flex-wrap gap-6">
+                <Avatar avatarSize="xl" src={`http://localhost:8800/uploads/${state.user?.profile_photo ?? "default.jpeg"}`}/>
+                <div className="w-fit flex flex-col gap-2">
+                    <h2 className="text-3xl font-mainBold">{ state.user?.username ?? "user" }</h2>
+                    <p className="text-base font-main font-semibold text-secondary">{ state.user?.email ?? "email" }</p>
+                </div>
+                <div className="w-full h-fit border-l-2 border-primary px-2">
+                    <p className="text-base font-main font-semibold">{ state.user?.user_bio ?? "Belum Ada Bio" }</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+function DataAndPrivacy() {
+    return (
+        <h1 className="text-3xl font-mainBold text-primary">Data & Privacy</h1>
+    )
+}
+
+function SettingAccount() {
+    return (
+        <h1 className="text-3xl font-mainBold text-primary">Setting Account</h1>
     )
 }
