d9157703fd958ba1c943537704f57ca852abd883
farrel irawan <irawanssfarrel@gmail.com>
2025-04-26 15:20:24 +0700

    update ui userprofile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -24,21 +24,17 @@ export default function UserProfile() {
     return (
         <>
             <section className="w-full h-screen flex">
-                <aside className="border-r border-secondary/30 w-64 p-5 h-full lg:w-1/6">
-                    <div className="w-full px-3">
-                        <h2 className="text-2xl font-mainBold text-primary leading-tight lg:text-3xl">Bunga <span className="block">Gampang.</span></h2>
-                    </div>
+                <aside className="border-r border-secondary/30 w-fit p-5 h-full lg:w-1/6">
                     <div className="h-fit w-full px-3 mt-8">
                         <ul className="flex flex-col gap-6">
                             { dataDashboard.map((data, index) => {
                                 return (
                                     <li 
-                                        className={`w-full flex items-center gap-3 cursor-pointer ${currentIndex === index ? 'text-primary' : 'text-secondary'}`} 
+                                        className={`w-fit border p-4 rounded-lg border-secondary/30 flex items-center gap-3 cursor-pointer ${currentIndex === index ? 'text-primary' : 'text-secondary'}`} 
                                         key={data.id}
                                         onClick={() => setCurrentIndex(index)}
                                     >
                                         <FontAwesomeIcon icon={data.icon} className="text-xl" />
-                                        <p className="text-base font-semibold font-main text-black">{ data.title }</p>
                                     </li>
                                 )
                             }) }
