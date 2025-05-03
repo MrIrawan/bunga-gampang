a0f3902feb0f30f63a2ee2d90f3007b11e493ed2
farrel irawan <irawanssfarrel@gmail.com>
2025-05-03 09:38:56 +0700

    modifikasi navbar blog

--- src/components/layout/BlogNavbar.jsx
+++ src/components/layout/BlogNavbar.jsx
@@ -8,8 +8,17 @@ import { AuthContext } from "../../context/AuthContext";
 export default function BlogNavbar() {
 
     const [isClicked, setIsClicked] = useState(false);
+    const [searchQuery, setSearchQuery] = useState("");
     const { state } = useContext(AuthContext);
     const isAuthorized = state.isAuthorized;
+
+
+    const handleSearch = (e) => {
+        const query = e.target.value;
+        setSearchQuery(query);
+        console.log(query);
+        
+    }
 
     return (
         <header className="w-full shadow-xl">
@@ -27,10 +36,11 @@ export default function BlogNavbar() {
                         id="search_query" 
                         className="border border-secondary/30 rounded-md pl-10 pr-3 py-2 w-full font-main font-semibold focus:outline-none focus:ring-2 focus:ring-primary transition-colors duration-300 ease-out"
                         placeholder="Cari artikel..."
+                        onChange={(e) => handleSearch(e)}
                     />
                 </div>
                 <div className="w-fit relative">
-                    <FontAwesomeIcon icon={faBars} className="text-3xl" onClick={() => setIsClicked(!isClicked)}/>
+                    <FontAwesomeIcon icon={faBars} className="text-3xl active:scale-75 transition-all duration-300 ease-out" onClick={() => setIsClicked(!isClicked)}/>
                     <div className={`absolute border border-secondary/30 rounded-md top-12 right-0 bg-white p-5 w-[300px] h-[250px] ${isClicked ? "flex flex-col justify-between" : "hidden"}`}>
                         <ul className="flex flex-col gap-3">
                             <li className="font-main text-base leading-relaxed hover-list">
@@ -47,7 +57,18 @@ export default function BlogNavbar() {
                             </li>
                         </ul>
                         <div className="w-full">
-                            <Avatar avatarSize="sm" src={`http://localhost:8800/uploads/${state.user?.profile_photo ?? "default.jpeg"}`}/>
+                            { isAuthorized ? (
+                                <Avatar avatarSize="sm" src={`http://localhost:8800/uploads/${state.user?.profile_photo ?? "default.jpeg"}`}/>
+                            ) : (
+                                <div className="w-full flex items-center justify-around">
+                                    <a href="/masuk">
+                                        <button className="px-4 py-1.5 border rounded text-base font-main bg-primary text-text">Masuk</button>
+                                    </a>
+                                    <a href="/daftar">
+                                        <button className="px-4 py-1.5 border rounded text-base font-main bg-secondary text-text">Daftar</button>
+                                    </a>
+                                </div>
+                            ) }
                         </div>
                     </div>
                 </div>
