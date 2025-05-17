59f12497a0c2f12c79a29d3061d5b29d3e001edd
farrel irawan <irawanssfarrel@gmail.com>
2025-05-18 06:30:16 +0700

    update UI card article

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,12 +1,18 @@
 import Card from "./ui/Card"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons"
+import ArticlesCard from "./ui/ArticlesCard"
 
 export default function Articles() {
     return(
         <>
         <div className="w-full mb-10">
             <h2 className="text-3xl font-mainBold text-primary capitalize leading-relaxed w-fit mx-auto">artikel terbaru bunga gampang</h2>
+        </div>
+        <div className="w-full p-3 flex items-center gap-3">
+            <ArticlesCard variant="large" classname={"hidden lg:block"}/>
+            <ArticlesCard variant="small"/>
+            <ArticlesCard variant="small" classname={"block lg:hidden"}/>
         </div>
             <div className="w-full p-3 flex items-center justify-center mt-24">
                 <div className="hover-list flex items-center gap-3 cursor-pointer">
--- src/components/ui/ArticlesCard.jsx
+++ src/components/ui/ArticlesCard.jsx
@@ -1,59 +1,56 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
-export default function ArticlesCard({ variant = 'default', data }) {
+export default function ArticlesCard({ variant = "small", classname }) {
     return (
         <>
-            <div key={data.id} className="shadow-lg border w-full h-[400px] flex lg:w-full">
-                {/* article image */}
-                <article className="w-3/5 h-full overflow-hidden lg:w-4/5">
-                    <img src={data.image_url} alt={`image-${data.id}`} className="w-full h-full object-cover"/>
-                </article>
-                {/* article content */}
-                <article className="w-full py-3 px-6 flex flex-col">
-                    {/* author and date */}
-                    <div className="w-fit flex gap-3 items-center mb-4 mt-3">
-                        <div className="flex items-center gap-2">
-                            <div className="w-8 h-8 bg-red-100 rounded-full overflow-hidden">
-                                <img src="https://placehold.co/600x400" alt="" className="w-full h-full object-cover"/>
-                            </div>
-                            <p className="text-sm font-main font-semibold text-secondary">Posted By { data.posted }</p>
-                        </div>
-                        <span className="w-2 h-2 bg-primary rounded-full"></span>
-                        <p className="text-sm font-main text-secondary font-semibold">On { new Date(data.created_at).toLocaleString("id-ID", {day: "numeric", year: "numeric", month: "long"}) }</p>
-                    </div>
-
-                    {/* title and description */}
-                    <div className="w-full flex flex-col mb-5">
-                        <h2 className="text-2xl font-mainBold mb-4 leading-tight">{ data.title }</h2>
-                        <p className="text-sm font-main text-secondary font-semibold lg:max-w-md">{ data.description }</p>
-                    </div>
-
-                    {/* for categories tags */}
-                    <div className="w-full lg:max-w-md">
-                        { Array.isArray(data.category.tags) ? data.category.tags.map((article, key) => (
-                            <span 
-                                key={key}
-                                className={`border border-secondary/60 py-1 px-5 mx-1 my-1 rounded-full text-base font-main cursor-pointer transition-all duration-200 ease-in-out`}
-                            >
-                                { article }
-                            </span>
-                        )) : <span 
-                                className={`border border-secondary/60 py-1 px-5 mx-1 my-1 rounded-full text-base font-main cursor-pointer transition-all duration-200 ease-in-out`}
-                            >
-                                { data.category.tags }
-                            </span> }
-                    </div>
-
-                    {/* read more button (footer) */}
-                    <div className="w-fit mt-auto mb-2">
-                        <a href="#" className="flex items-center gap-2 text-base font-main hover-list">
-                            Read More
-                            <FontAwesomeIcon icon={faArrowRight} />
-                        </a>
-                    </div>
-                </article>
-            </div>
+            <article className={`${classname} shadow-lg bg-text overflow-hidden relative group ${variant === "small" ? "w-[450px] h-[530px] flex flex-col" : "w-full h-96 lg:w-[70%] lg:h-[530px]"}`}>
+                { variant === "small" && smallArticleCard() }
+            </article>
         </>
     );
+}
+
+function smallArticleCard() {
+    return (
+        <>
+             {/* card header */}
+                <div className="w-full relative h-60 bg-green-500 overflow-hidden">
+                    <img src="https://placehold.co/300x200" alt="" className="w-full h-full object-cover"/>
+                    <span className="bg-text w-fit rounded-xl absolute top-3 left-3 py-0.5 px-3">
+                        <p className="text-sm font-main text-secondary font-semibold">8 Februari 2024</p>
+                    </span>
+                </div>
+                {/* card content (body) */}
+                <div className="w-full py-3 px-3">
+                    {/* user profile */}
+                    <div className="w-full flex items-center gap-2">
+                        <div className="w-10 h-10 overflow-hidden rounded-full">
+                            <img src="https://placehold.co/50x50" alt=""  className="w-full h-full object-cover"/>
+                        </div>
+                        <hr className="w-2 h-2 rounded-full bg-primary"/>
+                        <p className="text-sm font-main text-black font-semibold">Posted By Admin</p>
+                    </div>
+                    {/* article title */}
+                    <div className="mt-2">
+                        <h4 className="text-2xl font-mainBold text-black leading-relaxed capitalize">judul artikel</h4>
+                        <div className="flex flex-wrap w-full gap-1 mt-2">
+                            <span className="border border-secondary/30 py-0.5 px-5 my-1 rounded-full h-fit text-base font-main cursor-pointer">insight</span>
+                            <span className="border border-secondary/30 py-0.5 px-5 my-1 rounded-full h-fit text-base font-main cursor-pointer">finance</span>
+                            <span className="border border-secondary/30 py-0.5 px-5 my-1 rounded-full h-fit text-base font-main cursor-pointer">lifestyle</span>
+                            <span className="border border-secondary/30 py-0.5 px-5 my-1 rounded-full h-fit text-base font-main cursor-pointer">blockchain & crypto</span>
+                        </div>
+                    </div>
+                </div>
+                {/* card footer */}
+                <div className="w-full mt-auto py-4 px-3 bg-primary absolute bottom-0 translate-y-full transition-all duration-300 ease-in-out group-hover:translate-y-0">
+                    <a href="/">
+                        <button className="font-main text-text font-medium flex items-center">
+                            Baca Selengkapnya
+                            <FontAwesomeIcon icon={faArrowRight} className="ml-2"/>
+                        </button>
+                    </a>
+                </div>
+        </>
+    )
 }
